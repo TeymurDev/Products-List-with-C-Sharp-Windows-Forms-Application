8714124380f3cfc0b3402f8f0d8b3c8530c2df37
Teymur <you@example.com>
2020-01-17 02:48:02 +0400

    products list

---
+++ ProductsList/ProductsList/ProductsForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ProductsList
+{
+    partial class ProductsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgwProducts = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCategories = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgwProducts
+            // 
+            this.dgwProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwProducts.BackgroundColor = System.Drawing.Color.White;
+            this.dgwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgwProducts.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgwProducts.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgwProducts.Location = new System.Drawing.Point(0, 198);
+            this.dgwProducts.Name = "dgwProducts";
+            this.dgwProducts.Size = new System.Drawing.Size(800, 252);
+            this.dgwProducts.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.txtProductName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cmbCategories);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 115);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProductName.Location = new System.Drawing.Point(331, 57);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(197, 31);
+            this.txtProductName.TabIndex = 4;
+            this.txtProductName.TextChanged += new System.EventHandler(this.txtProductName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(360, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search Data";
+            // 
+            // cmbCategories
+            // 
+            this.cmbCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategories.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCategories.FormattingEnabled = true;
+            this.cmbCategories.Location = new System.Drawing.Point(64, 57);
+            this.cmbCategories.Name = "cmbCategories";
+            this.cmbCategories.Size = new System.Drawing.Size(218, 31);
+            this.cmbCategories.TabIndex = 2;
+            this.cmbCategories.SelectedIndexChanged += new System.EventHandler(this.cmbCategories_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Categories";
+            // 
+            // ProductsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgwProducts);
+            this.Name = "ProductsForm";
+            this.Text = "ProductsForm";
+            this.Load += new System.EventHandler(this.ProductsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwProducts;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbCategories;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ ProductsList/ProductsList/ProductsForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ProductsList
+{
+    public partial class ProductsForm : Form
+    {
+        private readonly string _connstring;
+        public ProductsForm()
+        {
+            InitializeComponent();
+            _connstring = ConfigurationManager.ConnectionStrings["main"].ConnectionString;
+        }
+
+        private void ProductsForm_Load(object sender, EventArgs e)
+        {
+            FillDataGridView();
+            FillCategoriesComboBox();
+        }
+
+        private void FillCategoriesComboBox()
+        {
+
+
+            using (var conn = new SqlConnection(_connstring))
+            {
+                string commandText = "select * from Categories";
+                using (var command = new SqlCommand(commandText, conn))
+                {
+                    conn.Open();
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        cmbCategories.Items.Add(new ComboItem
+                        {
+                            Id = 0,
+                            Name = "All"
+                        });
+
+
+                        while (reader.Read())
+                        {
+                            cmbCategories.Items.Add(new ComboItem
+                            {
+                                Id = (int)reader[0],
+                                Name = (string)reader[1]
+                            });
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void cmbCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboItem selectedCategory = (ComboItem)cmbCategories.SelectedItem;
+      
+                FillDataGridView(categoryId: selectedCategory.Id, fname: txtProductName.Text.Trim());
+
+
+        }
+
+        private void FillDataGridView(int? categoryId = 0, string fname = "")
+        {
+
+            using (SqlConnection conn = new SqlConnection(_connstring))
+            {
+                string commandText = "select Products.Id, ProductName, Categories.CategoryName " +
+                    "from Products " +
+                    "join Categories on Products.CategoryId = Categories.Id ";
+
+                if (categoryId == 0)
+                
+                    commandText += $" where Categories.Id = Categories.Id";
+                else
+                    commandText += $" where Categories.Id = {categoryId}";
+
+                commandText += $" and Products.ProductName LIKE '%{fname}%'";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(commandText, conn);
+
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dgwProducts.DataSource = dataTable;
+
+                dgwProducts.Columns[0].Visible = false;
+            }
+        }
+
+        private void txtProductName_TextChanged(object sender, EventArgs e)
+        {
+            ComboItem selectedCategory = (ComboItem)cmbCategories.SelectedItem;
+
+            FillDataGridView(categoryId: selectedCategory.Id, fname: txtProductName.Text.Trim());
+        }
+    }
+}
